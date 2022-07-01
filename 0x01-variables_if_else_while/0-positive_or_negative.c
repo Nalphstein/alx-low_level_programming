9a0a72897b2496a34cc6dcfca4a49a262d9c166d
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-01 05:37:42 -0700

    if and else statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,16 +11,16 @@
 
 int main(void)
 {
-	int j;
+	int n;
 
 	srand(time(0));
 
 	j = rand() - RAND_MAX / 2;
-	if (j == 0)
-		printf("%d is zero\n", j);
+	if (n == 0)
+		printf("%d is zero\n", n);
 	else if (j < 0)
-		printf("%d is negative\n", j);
+		printf("%d is negative\n", n);
 	else
-		printf("%d is positive\n", j);
+		printf("%d is positive\n", n);
 	return (0);
 }
