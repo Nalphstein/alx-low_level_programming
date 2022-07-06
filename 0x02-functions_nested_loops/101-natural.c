27e61778c905f86272b625b098d8fe44f5838dfd
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-05 18:46:52 -0700

    multiples of 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 1;
+	int count = 0;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0)
+			count += x;
+		else if (x % 5 == 0)
+			count += x;
+
+		x++;
+	}
+	printf("%d\n", count);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,27 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-	for (; n > 98; n--)
+		while (n >= 98)
 		{
-		printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
 	}
-	else if (n < 98)
+	else
 	{
-		for (; n < 98; n++)
+		while (n <= 98)
 		{
-		printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
 	}
-	printf("%d\n", n);
+	printf("\n");
 }
