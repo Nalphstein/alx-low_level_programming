bc504742159a01ac50af9b409ce257c7e6b18f91
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-07 16:00:41 -0700

    Print more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
+ * Description: Can only use _putchar twice.
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	c = 0;
+
+	while (c < 10)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+	_putchar('\n');
+}
+		
