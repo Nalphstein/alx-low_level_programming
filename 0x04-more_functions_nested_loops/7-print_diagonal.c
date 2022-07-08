4220c348cf614f9da083fb4b8e276d9b708f4f6b
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-07 17:14:35 -0700

    diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: takes in an integer
+ */
+
+void print_diagonal(int n)
+{
+	int i, x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+		for (x = 0; x <= i; x++)
+			putchar(' ');
+		putchar(92);
+		putchar('\n');
+		}
+	}
+}
