60075ad22fd01433b8bd98ddeab1d71fd002d107
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-12 07:42:59 -0700

    string of a length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,13 @@
 /**
  * _strlen - length of a string
  * @s: string
+ * Return: length of string
  */
 int _strlen(char *s)
 {
-	char = *s;
 	int c;
 
-	for(c = 0; s[c] != '\0'; c++);
+	for (c = 0; s[c] != '\0'; c++)
+		;
 	return (c);
 }
