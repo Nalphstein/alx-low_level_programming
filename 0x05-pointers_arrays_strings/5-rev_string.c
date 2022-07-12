d11b0dd4201826c4cf158e9a7d8814b090de8ba6
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-12 11:29:41 -0700

    Reverse string and no print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: character array string
+ */
+void rev_string(char *s)
+{
+	int c;
+	int i, 1;
+	char d;
+
+	for (c = 0; s[c] != '\0'; c++;)
+		;
+	1 = c;
+	for (c--; i = 0; i < 1 / 2; c--, i++)
+	{
+		d = s[i];
+s[i] = s[c];
+s[c] = d;
+	}
+}
