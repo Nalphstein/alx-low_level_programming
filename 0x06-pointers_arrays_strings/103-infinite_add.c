a09ac8f58938bf42ac987e7df7909adfa1838e7e
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-15 07:04:02 -0700

    infinite sdd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,75 @@
 #include "main.h"
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
- * infinite_add - adds two numbers
- * @n1: string with number 1
- * @n2: string with number 2
- * @r: buffer for new string of sum of n1 and n2
- * @size_r: size of a buffer
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer.
  *
- * Return: pointer to buffer
+ * Return: If r can store the sum - a pointer to the result.
+ * If r cannot store the sum - 0.
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+	else if (tens && r_index < 0)
+		return (0);
+	return (r + r_index + 1);
+}
+/**
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: The buffer to store the result.
+ * @size_r: The buffer size.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ * If r cannot store the sum - 0.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, j;
-	int carry;
+	int index, n1_len = 0, n2_len = 0;
 
-	l1 = _strlen(n1);
-	l2 = _strlen(n2);
-	if (l1 > size_r - 1 || l2 > size_r - 1)
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
-	carry = 0;
-	j = 0;
-	l1--;
-	l2--;
-	while (l1 >= 0 || l2 >= 0)
-	{
-		carry = _carry(l1, l2, n1, n2, r, j, carry);
-		l1--;
-		l2--;
-		j++;
-	}
-	if (carry == 1)
-	{
-		r[j] = carry + '0';
-		r[j + 1] = '\0';
-		if (_strlen(r) + 1 > size_r) /* _strlen(r) + '\0' */
-			return (0);
-	}
-	else
-		r[j] = '\0';
-	rev_string(r);
-	return (r);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+
+	return (add_strings(n1, n2, r, --size_r));
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,68 +2,43 @@
 #include <stdio.h>
 
 /**
- * _min - get minimum
- * @a: an int
- * @b: an int
- * Return: min value, a if tie
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ * the byte position, then showing the hex content,
+ * then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
  */
-/**
- * _min - get minimum
- * @a: an int
- * @b: an int
- * Return: min value, a if tie
- */
-int _min(int a, int b)
-{
-	if (a <= b)
-		return (a);
-	return (b);
-}
-
-
-
-char printable(char a)
-{
-	if (a >= ' ' && a <= '~')
-		return (a);
-	return ('.');
-}
-
 void print_buffer(char *b, int size)
 {
-	int i, j, res;
+	int byte, index;
 
-	i = 0;
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + index + byte));
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 &&
+				*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
+		printf("\n");
+	}
 	if (size <= 0)
 		printf("\n");
-	while (i < size)
-	{
-		printf("%8.8x:", i);
-		res = _min(10, size - i);
-		j = 0;
-		while (j < res)
-		{
-			if (j % 2 == 0)
-				printf(" ");
-			printf("%02x", *(b + i + j));
-			j++;
-		}
-		while (j < 10)
-		{
-			if (j % 2 == 0)
-				printf(" ");
-			printf("  ");
-			j++;
-		}
-		printf(" ");
-		j = 0;
-		while (j < res)
-		{
-			printf("%c", printable(*(b + i + j)));
-			j++;
-		}
-		printf("\n");
-		i += 10;
-	}
-	return (res)
 }
