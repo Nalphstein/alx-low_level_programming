a94e184710f8d515cc75a4ff1d995a9122cfaaef
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-07-15 06:46:14 -0700

    infinite sdd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,43 @@
 #include "main.h"
 
 /**
- * infinite_add -  adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: result
- * @size_r: result lenght
- * Return: sum
+ * infinite_add - adds two numbers
+ * @n1: string with number 1
+ * @n2: string with number 2
+ * @r: buffer for new string of sum of n1 and n2
+ * @size_r: size of a buffer
  *
+ * Return: pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+	int l1, l2, j;
+	int carry;
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	if (i > j)
-		l = i;
-	else
-		l = j;
-	if (l + 1 > size_r)
+	l1 = _strlen(n1);
+	l2 = _strlen(n2);
+	if (l1 > size_r - 1 || l2 > size_r - 1)
 		return (0);
-	r[l] = '\0';
-	for (k = l - 1 ; k >= 0 ; k--)
+	carry = 0;
+	j = 0;
+	l1--;
+	l2--;
+	while (l1 >= 0 || l2 >= 0)
 	{
-		i--;
-		j--;
-		if (i >= 0)
-			f = n1[i] - '0';
-		else
-			f = 0;
-		if (j >= 0)
-			s = n2[j] - '0';
-		else
-			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
+		carry = _carry(l1, l2, n1, n2, r, j, carry);
+		l1--;
+		l2--;
+		j++;
 	}
-	if (d == 1)
+	if (carry == 1)
 	{
-		r[l + 1] = '\0';
-		if (l + 2 > size_r)
+		r[j] = carry + '0';
+		r[j + 1] = '\0';
+		if (_strlen(r) + 1 > size_r) /* _strlen(r) + '\0' */
 			return (0);
-		while (l-- >= 0)
-			r[l + 1] = r[l];
-		r[0] = d + '0';
 	}
+	else
+		r[j] = '\0';
+	rev_string(r);
 	return (r);
 }
