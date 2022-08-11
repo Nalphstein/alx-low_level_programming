f0c194840a6df6fb8f551893c1b3c275df7e1961
Mohammed Ali Mohammed <djmohams123@gmail.com>
2022-08-11 16:21:24 -0700

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,59 +1,20 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
-
+#include <stdlib.h>
 /**
- * _strlen - get length of a string
- * @s: a string
- * Return: length of string
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
  */
-int _strlen(const char *s)
+void free_list(list_t *head)
 {
-	int i;
+	list_t *tmp;
 
-	for (i = 0; *(s + i); ++i)
-		;
-	return (i);
-}
-
-
-/**
- * add_node_end - add a node to the endo of the list
- * @head: pointer to the list
- * @str: value to add
- * Return: address of new element or NULL if failed
- */
-list_t *add_node_end(list_t **head, const char *str)
-{
-
-	list_t *new, *tmp;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL || str == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
+	while (head)
 	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(str);
-	new->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
 	}
-	tmp = *head;
-	while (tmp != NULL)
-	{
-		if (tmp->next == NULL)
-		{
-			tmp->next = new;
-			return (new);
-		}
-		tmp = tmp->next;
-	}
-	return (new);
+
 }
